8afd9cc51fcc0bc9bc2a7bb93ad7deb587029e4f
vrushaleeabhale <77379301+vrushaleeabhale@users.noreply.github.com>
2021-01-13 12:56:47 +0530

    Add files via upload

---
+++ Permutation.java
@@ -0,0 +1,32 @@
+package Assignments;
+
+public class Permutation 
+{
+	    static void permute(int[] a, int k) 
+	    {
+	        if (k == a.length) 
+	        {
+	            for (int i = 0; i < a.length; i++) 
+	            {
+	                System.out.print(" [" + a[i] + "] ");
+	            }
+	            System.out.println();
+	        } 
+	        else 
+	        {
+	            for (int i = k; i < a.length; i++) 
+	            {
+	                int temp = a[k];
+	                a[k] = a[i];
+	                a[i] = temp;
+	 
+	                permute(a, k + 1);
+	 
+	                temp = a[k];
+	                a[k] = a[i];
+	                a[i] = temp;
+	            }
+	        }
+	    }
+
+}
